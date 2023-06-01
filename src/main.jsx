838c3cad92e986c31f154b67d912cada87efa16d
Julian Babon <babonjmc@gmail.com>
2023-06-01 19:41:55 -0400

    initial setup of login.jsx and login.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './pages/App.jsx'
 import './index.css'
 import SignupPage from './pages/Signup.jsx'
+import LoginPage from './pages/Login.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App />  */}
-    <SignupPage/>
+    {/* <SignupPage/> */}
+    <LoginPage/>
   </React.StrictMode>,
 )
