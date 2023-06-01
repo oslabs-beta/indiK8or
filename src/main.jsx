70b883c2244a9fd968db9a00ebf3acccea9d63e5
Julian Babon <babonjmc@gmail.com>
2023-05-31 22:42:42 -0400

    initial setup signup.jsx and signup.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './pages/App.jsx'
 import './index.css'
+import SignupPage from './pages/Signup.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App />  */}
+    <SignupPage/>
   </React.StrictMode>,
 )
