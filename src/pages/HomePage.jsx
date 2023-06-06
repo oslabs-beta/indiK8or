12b9b59e60403518b92e152c525fa249051ef365
LeRocque <t.lerocque@gmail.com>
2023-06-06 16:02:50 -0400

    blocked-access-to-homepage-if-not-logged-in

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,8 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Navigation from '../components/Navigation.jsx'
 import Dashboard from '../components/Dashboard.jsx'
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const darkTheme = createTheme({
   palette: {
@@ -12,10 +13,34 @@ const darkTheme = createTheme({
 });
 
 const HomePage = () => {
-
+  const navigate = useNavigate();
   const [dashboardClicked, setDashboardClicked] = useState(false);
   const handleDashboard = () => setDashboardClicked(true);
   
+  useEffect(() => {
+    fetch('http://localhost:4000/login/isLoggedIn', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // include cookies from cross origin request
+        credentials: 'include',
+        body: JSON.stringify({}),
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 303) {
+          // Handle success response
+          // Update the state to indicate user creation success
+          alert('You must be logged in to view this page');
+          navigate('/login/loginRequest');
+        }
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error(error);
+      });
+  }, [navigate]);
 
   return (
     <ThemeProvider theme={darkTheme}>
