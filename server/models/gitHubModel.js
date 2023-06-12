23b5f34fa1286fa56f7217cc00d3ee0d9b2d55cd
LeRocque <t.lerocque@gmail.com>
2023-06-12 00:15:47 -0400

    added createdAt to GitUser Schema

--- server/models/gitHubModel.js
+++ server/models/gitHubModel.js
@@ -4,6 +4,18 @@ const Schema = mongoose.Schema;
 
 const gitSchema = new Schema({
   username: { type: String, required: true, unique: true },
+  createdAt: {
+    type: String,
+    default: () => {
+      const date = new Date();
+      return `${
+        date.getMonth() + 1
+      }/${date.getDate()}/${date.getFullYear()} ${date
+        .getHours()
+        .toString()
+        .padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
+    },
+  },
 });
 
 const GitUser = mongoose.model('gituser', gitSchema);
