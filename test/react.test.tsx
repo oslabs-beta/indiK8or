1fa639a7d2e3dae4e945decd14aaba825007da6e
LeRocque <t.lerocque@gmail.com>
2023-06-12 21:02:38 -0400

    ts running

--- test/react.test.tsx
+++ test/react.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, waitFor } from '@testing-library/react';
-import HomePage from '../src/pages/HomePage.jsx';
+import HomePage from '../src/pages/HomePage.js';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('sidebar', () => {
