16ab735224d6c5d0d922791a9f3c78509077bcca
Julian Babon <babonjmc@gmail.com>
2023-06-13 22:18:22 -0400

    signup page converted to ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,19 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   test: {
-//     globals: true,
-//     environment: 'jsdom',
-//     setupFiles: '/test/setup.js',
-//   },
-//   server: {
-//     port: 5000,
-//     proxy: {
-//       '/login/**': { target: 'http://localhost:4000/' },
-//       '/auth': 'http://localhost:4000/',
-//     },
-//   },
-// });
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: '/test/setup.ts',
+  },
+  server: {
+    port: 5000,
+    proxy: {
+      '/login/**': { target: 'http://localhost:4000/' },
+      '/auth': 'http://localhost:4000/',
+    },
+  },
+});
+
