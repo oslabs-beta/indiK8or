d09cfb3873903f8daff8359547da957c3fc2dab8
LeRocque <t.lerocque@gmail.com>
2023-06-15 16:27:21 -0400

    Pages fully converted to TS and have consistent syntax

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,26 +3,27 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Dashboard from '../components/Dashboard.tsx';
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
 import Sidebar from '../components/Sidebar.tsx';
 import '../css/HomePage.css';
+import { LightDarkTheme } from '../../types.ts';
 
 const HomePage = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [dashboardClicked, setDashboardClicked] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
-  const [userId, setUserId] = useState(null);
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [dashboardClicked, setDashboardClicked] = useState<boolean>(false);
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const [userId, setUserId] = useState<string>('');
 
-  const navigate = useNavigate();
+  const navigate: NavigateFunction = useNavigate();
 
   // Set the value of dashboardClicked to the opposite of its current value using func
-  const handleDashboard = () =>
+  const handleDashboard = ():void =>
     setDashboardClicked((prevDashboardClicked) => !prevDashboardClicked);
 
   // check to see if user is logged in, if they are we set loggedIn to true and render ThemeProvider
   // if they are not logged in we will receive a 303 and send them to the loginPage
   useEffect(() => {
-    const checkLoggedIn = async () => {
+    const checkLoggedIn = async (): Promise<void> => {
       try {
         const response = await fetch('http://localhost:4000/login/isLoggedIn', {
           method: 'POST',
@@ -37,7 +38,7 @@ const HomePage = () => {
           alert('You must be logged in to view this page');
           navigate('/login/loginRequest');
         } else {
-          const userId = await response.json();
+          const userId: string = await response.json();
           setUserId(userId);
           setLoggedIn(true);
         }
@@ -50,7 +51,7 @@ const HomePage = () => {
     checkLoggedIn();
   }, [navigate]);
 
-  const darkTheme = createTheme({
+  const darkTheme: LightDarkTheme = createTheme({
     palette: {
       mode: darkMode ? 'dark' : 'light',
       background: darkMode ? { default: '#212529' } : { default: '#faf3dd' },
