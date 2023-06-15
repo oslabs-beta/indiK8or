366c2cec70aae3f628925148780b6f4a4ac4e896
Julian Babon <babonjmc@gmail.com>
2023-06-15 17:23:11 -0400

    convert test to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
+import { ReactElement } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import HomePage from './pages/HomePage';
 import WelcomePage from './pages/WelcomePage';
 
-function App() {
+function App(): ReactElement {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
--- test/welcomePage.test.tsx
+++ test/welcomePage.test.tsx
@@ -2,6 +2,7 @@ import { expect, test} from 'vitest'
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from "react-router-dom";
 import WelcomePage from '../src/pages/WelcomePage';
+import React from 'react'
 
 test('renders welcome page correctly', () => {
     render(
@@ -12,10 +13,10 @@ test('renders welcome page correctly', () => {
 
     // Assert Header and button links work
     expect(screen.getByText('Introducing indiK8or: Simplify and Visualize Your Kubernetes Environment')).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: 'Sign in' })).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: 'Sign up' })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Ivy Wang' })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Julian Babon' })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Yueran Li' })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Tadd LeRocque' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Sign in' }) as HTMLInputElement).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Sign up' }) as HTMLInputElement).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Ivy Wang' }) as HTMLInputElement).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Julian Babon' }) as HTMLInputElement).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Yueran Li' }) as HTMLInputElement).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Tadd LeRocque' }) as HTMLInputElement).toBeInTheDocument();
   });
