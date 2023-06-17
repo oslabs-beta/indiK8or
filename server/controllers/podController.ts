f3b4d3425c0dce6a33b972640a4b8fe808e8c7e0
LeRocque <t.lerocque@gmail.com>
2023-06-17 12:44:11 -0400

    changing

--- server/controllers/podController.ts
+++ server/controllers/podController.ts
@@ -22,13 +22,13 @@ console.log('INSIDE GETPODS MIDDLEWARE');
       console.log('lines are: ', lines);
       const headers: string[] = lines[0].split(/\s{2,}/);
       console.log('headers are:', headers);
-      const results: any[] = [];
+      const results: PodRow[] = [];
 
       for (let i = 1; i < lines.length; i++) {
         const values: string[] = lines[i].split(/\s{2,}/);
         if (values.length === headers.length) {
 
-          const pod: any = {};
+          const pod: PodRow = {} as PodRow;
 
           for (let j = 0; j < headers.length; j++) {
             pod[headers[j]] = values[j];
