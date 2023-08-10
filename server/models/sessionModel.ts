63853396c01bdaf19ed7b1726b04152060a6cacf
LeRocque <t.lerocque@gmail.com>
2023-08-09 23:06:50 -0400

    fixed broken logo while in production mode, plus moved bcrypt login into createUser

--- server/models/sessionModel.ts
+++ server/models/sessionModel.ts
@@ -19,6 +19,6 @@ const sessionSchema = new Schema({
   createdAt: { type: Date, expires: 300, default: Date.now() },
 });
 //ensure that the "sessions" collection only retains documents for a specified period of time
-sessionSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60000 });
+sessionSchema.index({ createdAt: 1 }, { expireAfterSeconds: 300 });
 
 export const Session = mongoose.model("Session", sessionSchema);
