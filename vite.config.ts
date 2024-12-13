1241d472648a84327bd7531be464fe39918ecb6d
Ivy Wang <wanwangbtb@gmail.com>
2024-12-13 11:11:37 -0800

    fixed proxy config for login

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,13 @@ export default defineConfig({
   server: {
     port: 5000,
     proxy: {
-      "/login": "http://localhost:4000/",
+      // "/login": "http://localhost:4000/",
+      '/login': {
+        target: 'http://your-backend-server.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/login/, ''),
+      },
       "/logout": "http://localhost:4000/",
       "/dashboard": "http://localhost:4000/",
       "/pod": "http://localhost:4000/",
